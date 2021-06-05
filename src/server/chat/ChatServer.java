f08dceb42c7af9cd1ea583df34772de257a03695
alireza-nasirian <an1380an@gmail.com>
2021-06-05 11:45:24 +0430

    feat: add startChat method

--- src/server/chat/ChatServer.java
+++ src/server/chat/ChatServer.java
@@ -2,11 +2,27 @@ package server.chat;
 
 import java.util.ArrayList;
 
+/**
+ * The  Chat server class.
+ */
 public class ChatServer {
 
     private ArrayList<UserThread> userThreads = new ArrayList<>();
     private int seconds;
 
+    /**
+     * Start chat method starts chat with given members and time.
+     *
+     * @param userThreads is members ti chat
+     * @param seconds     is time of chatting
+     */
+    public void startChat(ArrayList<UserThread> userThreads, int seconds){
 
+        this.seconds = seconds;
+        this.userThreads = userThreads;
+        for (UserThread u : userThreads){
+            u.start();
+        }
+    }
 
 }
