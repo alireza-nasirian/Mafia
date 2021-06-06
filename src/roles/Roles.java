98091b8077b4ec6bbb22d2d1aa2878ed926ac4d6
alireza-nasirian <an1380an@gmail.com>
2021-06-06 10:49:56 +0430

    feat: add Roles Enum

---
+++ src/roles/Roles.java
@@ -0,0 +1,14 @@
+package roles;
+
+public enum Roles {
+    GODFATHER,
+    DR_LECTER,
+    SIMPLE_MAFIA,
+    CITY_DOCTOR,
+    DETECTIVE,
+    PROFESSIONAL,
+    MAYOR,
+    PSYCHOLOGIST,
+    DIE_HARD,
+    SIMPLE_CITIZEN;
+}
