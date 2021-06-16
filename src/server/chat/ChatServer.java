623fe5e6c79ee6cf28b48fb3ec8c70e25d10008b
alireza-nasirian <an1380an@gmail.com>
2021-06-16 11:23:30 +0430

    fix: add god parameter to constructor

--- src/server/chat/ChatServer.java
+++ src/server/chat/ChatServer.java
@@ -1,27 +1,48 @@
 package server.chat;
 
+import roles.Mafia;
+import roles.Person;
+
+import java.io.IOException;
 import java.util.ArrayList;
+
+import server.God;
 
 /**
  * The  Chat server class.
+ *
+ * @author Alireza Nasirian
+ * @version 1.3
  */
 public class ChatServer {
 
-    private ArrayList<UserThread> userThreads = new ArrayList<>();
+    private static ArrayList<Person> talkerUsers = new ArrayList<>();
+    private static ArrayList<Person> listenerUsers = new ArrayList<>();
     private int seconds;
+    protected God god;
+
+    public ChatServer(God god) {
+        this.god = god;
+    }
 
     /**
      * Start chat method starts chat with given members and time.
      *
-     * @param userThreads is members ti chat
-     * @param seconds     is time of chatting
+     * @param talkers   is members to chat
+     * @param listeners can read chats
+     * @param seconds   is time of chatting
      */
-    public void startChat(ArrayList<UserThread> userThreads, int seconds) {
+    public void startChat(ArrayList<Person> talkers, ArrayList<Person> listeners, int seconds) {
 
         this.seconds = seconds;
-        this.userThreads = userThreads;
-        for (UserThread u : userThreads) {
-            u.start();
+
+        setTalkerUsers(talkers);
+        setListenerUsers(listeners);
+        ArrayList<UserThread> userThreads = new ArrayList<>();
+        for (Person person : talkers) {
+            UserThread userThread = new UserThread(this, person.getOutput(), person.getInput(), person.getUsername());
+            person.userThread = userThread;
+            userThread.start();
         }
     }
 
@@ -30,9 +51,21 @@ public class ChatServer {
      *
      * @param message is message to be sent
      */
-    protected void broadcast(String message) {
-        for (UserThread u : userThreads) {
-            u.sendMessage(message);
+    public void broadcast(String message) {
+        for (Person person : listenerUsers) {
+            try {
+                person.getUserThread().sendMessage(message);
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+                god.alive_persons.remove(person);
+                god.observers.remove(person);
+                if (person instanceof Mafia) {
+                    god.mafias.remove(person);
+                } else {
+                    god.citizens.remove(person);
+                }
+                god.dead.add(person);
+            }
         }
     }
 
@@ -41,5 +74,27 @@ public class ChatServer {
      */
     public int getSeconds() {
         return seconds;
+    }
+
+
+    /**
+     * Sets listener users.
+     *
+     * @param listeners the listeners
+     */
+    public void setListenerUsers(ArrayList<Person> listeners) {
+        listenerUsers = new ArrayList<>();
+        listenerUsers = listeners;
+    }
+
+
+    /**
+     * Sets talker users.
+     *
+     * @param talkers the talkers
+     */
+    public void setTalkerUsers(ArrayList<Person> talkers) {
+        talkerUsers = new ArrayList<>();
+        talkerUsers = talkers;
     }
 }
